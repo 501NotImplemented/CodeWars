af66d0616cf7c309926ebbc342db6510e60f81f0
Oleksandr Shyian <ashqa14@gmail.com>
2022-12-06 18:42:01 +0200

    Initial commit

---
+++ MidEndianNumbers/Kata.cs
@@ -0,0 +1,33 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+Console.WriteLine(Kata.MidEndian(0));
+
+internal class Kata
+{
+    public static string MidEndian(long n)
+    {
+        var hexValueInBigEndian = n.ToString("X");
+
+        if (hexValueInBigEndian.Length == 1)
+        {
+            hexValueInBigEndian = $"0{hexValueInBigEndian}";
+        }
+
+        var leftPart = string.Empty;
+        var middlePart = string.Empty;
+        var rightPart = string.Empty;
+
+        string midEndian;
+
+        if (hexValueInBigEndian.Length == 2)
+        {
+            midEndian = hexValueInBigEndian;
+        }
+        else
+        {
+            midEndian = $"{leftPart}{middlePart}{rightPart}";
+        }
+
+        return midEndian;
+    }
+}
