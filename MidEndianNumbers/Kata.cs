f6d065deee0c3c87dea1a41c121ce768be18ed54
Oleksandr Shyian <ashqa14@gmail.com>
2022-12-06 20:57:52 +0200

    Add Pad0ForSingleDigits

--- MidEndianNumbers/Kata.cs
+++ MidEndianNumbers/Kata.cs
@@ -1,33 +1,52 @@
 ﻿// See https://aka.ms/new-console-template for more information
 
-Console.WriteLine(Kata.MidEndian(0));
+using System.Text;
+
+Console.WriteLine(Kata.MidEndian(168496141));
 
 internal class Kata
 {
     public static string MidEndian(long n)
     {
         var hexValueInBigEndian = n.ToString("X");
+        string[] hexSplitIntoPairs = SplitString(hexValueInBigEndian);
 
-        if (hexValueInBigEndian.Length == 1)
+        Pad0ForSingleDigits(hexSplitIntoPairs);
+
+        var sb = new StringBuilder();
+        for (var index = 0; index < hexSplitIntoPairs.Length; index++)
         {
-            hexValueInBigEndian = $"0{hexValueInBigEndian}";
+            string pair = hexSplitIntoPairs[index];
+            sb.Append(pair);
         }
 
-        var leftPart = string.Empty;
-        var middlePart = string.Empty;
-        var rightPart = string.Empty;
+        var midEndian = sb.ToString();
 
-        string midEndian;
-
-        if (hexValueInBigEndian.Length == 2)
-        {
-            midEndian = hexValueInBigEndian;
-        }
-        else
-        {
-            midEndian = $"{leftPart}{middlePart}{rightPart}";
-        }
-
+        // 0D0B0A0C
         return midEndian;
+    }
+
+    private static void Pad0ForSingleDigits(string[] inputHex)
+    {
+        for (var index = 0; index < inputHex.Length; index++)
+        {
+            string pair = inputHex[index];
+            if (pair.Length == 1)
+            {
+                inputHex[index] = $"0{pair}";
+            }
+        }
+    }
+
+    private static string[] SplitString(string input)
+    {
+        var output = new string[input.Length / 2 + (input.Length % 2 == 0 ? 0 : 1)];
+
+        for (var i = 0; i < output.Length; i++)
+        {
+            output[i] = input.Substring(i * 2, i * 2 + 2 > input.Length ? 1 : 2);
+        }
+
+        return output;
     }
 }
