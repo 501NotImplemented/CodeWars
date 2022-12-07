b857da065ae7b593f41b68a817977ff962ff3850
Oleksandr Shyian <ashqa14@gmail.com>
2022-12-07 23:17:13 +0200

    Add basic solution

--- MidEndianNumbers/Kata.cs
+++ MidEndianNumbers/Kata.cs
@@ -2,8 +2,9 @@
 
 using System.Text;
 
-Console.WriteLine(Kata.MidEndian(168496141));
+Console.WriteLine(Kata.MidEndian(658188));
 
+//http://catb.org/jargon/html/M/middle-endian.html
 internal class Kata
 {
     public static string MidEndian(long n)
@@ -12,18 +13,79 @@ internal class Kata
         string[] hexSplitIntoPairs = SplitString(hexValueInBigEndian);
 
         Pad0ForSingleDigits(hexSplitIntoPairs);
+        Swap0AndAddress(hexSplitIntoPairs);
 
-        var sb = new StringBuilder();
-        for (var index = 0; index < hexSplitIntoPairs.Length; index++)
+        var midEndianBuilder = new StringBuilder();
+        var middlePairIndex = 0;
+        string middlePair = hexSplitIntoPairs[middlePairIndex];
+
+        var leftPairStartIndex = 1;
+        var rightPairStartIndex = 2;
+
+        var leftPairBuilder = new StringBuilder();
+        var rightPairBuilder = new StringBuilder();
+
+        if (hexSplitIntoPairs.Length > 1)
         {
-            string pair = hexSplitIntoPairs[index];
-            sb.Append(pair);
+            IEnumerable<int> leftIndexes = GetLeftIndexes(leftPairStartIndex, hexSplitIntoPairs.Length - 1);
+            foreach (int leftIndex in leftIndexes)
+            {
+                leftPairBuilder.Append(hexSplitIntoPairs[leftIndex]);
+            }
+
+            IEnumerable<int> rightIndexes = GetRightIndexes(rightPairStartIndex, hexSplitIntoPairs.Length - 1);
+            foreach (int rightIndex in rightIndexes)
+            {
+                rightPairBuilder.Append(hexSplitIntoPairs[rightIndex]);
+            }
         }
 
-        var midEndian = sb.ToString();
+        var leftPair = leftPairBuilder.ToString();
+        var rightPair = rightPairBuilder.ToString();
 
-        // 0D0B0A0C
+        midEndianBuilder.Append(leftPair);
+        midEndianBuilder.Append(middlePair);
+        midEndianBuilder.Append(rightPair);
+
+        var midEndian = midEndianBuilder.ToString();
+
+        // 0B0A0C 658188 1-0-2
+        // 0D0B0A0C 168496141 3-1-0-2
+        // 0D0B0A0C0E 43135012110 3-1-0-2-4
+        // 96987F 9999999 1-0-2
         return midEndian;
+    }
+
+    private static bool GetEven(int number)
+    {
+        if (number % 2 == 0)
+        {
+            return true;
+        }
+
+        return false;
+    }
+
+    private static IEnumerable<int> GetLeftIndexes(int startIndex, int endIndex)
+    {
+        IEnumerable<int> result = Enumerable.Range(startIndex, endIndex).Where(GetOdd).Select(i => i);
+        return result;
+    }
+
+    private static bool GetOdd(int number)
+    {
+        if (number % 2 != 0)
+        {
+            return true;
+        }
+
+        return false;
+    }
+
+    private static IEnumerable<int> GetRightIndexes(int startIndex, int endIndex)
+    {
+        IEnumerable<int> result = Enumerable.Range(startIndex, endIndex).Where(GetEven).Select(i => i);
+        return result;
     }
 
     private static void Pad0ForSingleDigits(string[] inputHex)
@@ -48,5 +110,27 @@ internal class Kata
         }
 
         return output;
+    }
+
+    private static void Swap0AndAddress(string[] inputHex)
+    {
+        for (var index = 0; index < inputHex.Length; index++)
+        {
+            char[] initialPair = inputHex[index].ToCharArray();
+            if (initialPair.Contains('0'))
+            {
+                char initialFirstChar = initialPair[0];
+
+                if (initialFirstChar != '0')
+                {
+                    char[] swappedChars =
+                        {
+                            '0', initialFirstChar
+                        };
+
+                    inputHex[index] = new string(swappedChars);
+                }
+            }
+        }
     }
 }
