134db428fee5948aac9e812c76634a17924ba914
Oleksandr Shyian <ashqa14@gmail.com>
2022-12-18 19:14:50 +0200

    Fix 0 test

--- MidEndianNumbers/Kata.cs
+++ MidEndianNumbers/Kata.cs
@@ -2,7 +2,7 @@
 
 using System.Text;
 
-Console.WriteLine(Kata.MidEndian(168496141));
+Console.WriteLine(Kata.MidEndian(658188));
 
 //http://catb.org/jargon/html/M/middle-endian.html
 internal class Kata
@@ -17,39 +17,44 @@ internal class Kata
 
         var midEndianBuilder = new StringBuilder();
         char[] hexChars = string.Concat(hexSplitIntoPairs).ToCharArray();
-        var middleCharIndex = 0; // GetMiddlePairIndex(hexChars);
+        var middleCharIndex = 0;
         Console.WriteLine($"Middle char index is {middleCharIndex}");
         var middleChar = Convert.ToString(hexChars[middleCharIndex]);
 
-        // midEndianBuilder.Append(middleChar);
         string midEndian;
 
-        if (hexChars.Length > 1)
+        if (hexChars.Length > 2)
         {
             Dictionary<string, List<int>> indexes = GetIndexes(hexChars);
 
-            string leftPair = GetLeftPair(hexChars);
+            List<int> leftIndexes = indexes["Left"];
+            string leftPair = GetLeftPair(hexChars, leftIndexes);
 
-            //// 96987F
-            //// act 698
             midEndianBuilder.Append(leftPair);
 
             Console.Write(middleCharIndex);
             midEndianBuilder.Append(middleChar);
 
-            string rightPair = GetRightPair(hexChars, middleCharIndex);
+            List<int> rightIndexes = indexes["Right"];
+            string rightPair = GetRightPair(hexChars, rightIndexes);
             midEndianBuilder.Append(rightPair);
-            midEndian = midEndianBuilder.ToString();
 
             // 69867F
             // 0D0B0A0C
             // 0D0B0A0C0E
+            // 0B0A0C 658188
         }
         else
         {
-            midEndian = middleChar;
+            foreach (char hexChar in hexChars)
+            {
+                midEndianBuilder.Append(hexChar);
+            }
         }
 
+        midEndian = midEndianBuilder.ToString();
+
+        // 2-3-0-1-4-5 658188
         // 2-3-0-1-4-5 9999999
         // 6-7-2-3-0-1-4-5 168496141
         // 6-7-2-3-0-1-4-5-8-9 43135012110
@@ -68,7 +73,7 @@ internal class Kata
     {
         Stack<int> evenIndexes = GetEvenIndexes(input);
         Stack<int> oddIndexes = GetOddIndexes(input);
-
+        Queue<int> reversedOddQue = new();
         Dictionary<string, List<int>> indexes = new()
                                                     {
                                                         {
@@ -78,139 +83,91 @@ internal class Kata
                                                             "Right", new List<int>()
                                                         }
                                                     };
-
-        // 6-7-2-3-0-1-4-5
-        for (var i = 0; i < input.Length; i++)
+        if (IsEven(input.Length))
         {
-            if (evenIndexes.Count > 0)
+            Console.WriteLine($"Input length is {input.Length}");
+            for (var i = 0; i < input.Length; i++)
             {
-                if (IsEven(i))
+                if (evenIndexes.Count > 0)
                 {
-                    indexes["Left"].Add(evenIndexes.Pop());
-                    indexes["Right"].Add(oddIndexes.Pop());
+                    if (IsOdd(i))
+                    {
+                        IEnumerable<int> reversedOddCollection = oddIndexes.Reverse();
+                        reversedOddQue = new Queue<int>(reversedOddCollection);
+
+                        indexes["Left"].Add(reversedOddQue.Dequeue());
+                        indexes["Right"].Add(evenIndexes.Pop());
+                    }
+                    else
+                    {
+                        indexes["Left"].Add(evenIndexes.Pop());
+                        if (i == 0 && reversedOddQue.Count == 0)
+                        {
+                            indexes["Right"].Add(oddIndexes.Pop());
+                        }
+                        else
+                        {
+                            indexes["Right"].Add(reversedOddQue.Dequeue());
+                        }
+                    }
                 }
                 else
                 {
-                    IEnumerable<int> reversedOddCollection = oddIndexes.Reverse();
-                    Queue<int> reversedOddQue = new(reversedOddCollection);
-                    oddIndexes = new Stack<int>(reversedOddQue);
-                    indexes["Left"].Add(reversedOddQue.Dequeue());
-
-                    // IEnumerable<int> reversedEvenCollection = evenIndexes.Reverse();
-                    // Queue<int> reversedEvenQue = new(reversedEvenCollection);
-                    // evenIndexes = new Stack<int>(reversedEvenQue);
-                    indexes["Right"].Add(evenIndexes.Pop());
+                    if (reversedOddQue.Count > 0)
+                    {
+                        indexes["Left"].Add(reversedOddQue.Dequeue());
+                    }
+                    else
+                    {
+                        break;
+                    }
                 }
             }
-            else
-            {
-                Console.WriteLine($"Indexes left: {oddIndexes.Count}");
-            }
+        }
+        else
+        {
+            throw new NotImplementedException();
         }
 
+        // 6-7-2-3-0-1-4-5 168496141
+        // 2-3-0-1-4-5 658188
+        // 6-7-2-3-0-1-4-5-8-9 43135012110
+        List<int> expectedRightIndexes = new()
+                                             {
+                                                 1,
+                                                 4,
+                                                 5,
+                                                 8,
+                                                 9
+                                             };
+
+        List<int> actualRightIndexes = indexes["Right"];
+
+        // CollectionAssert.AreEqual(expectedRightIndexes, actualRightIndexes);
+        List<int> expectedLeftIndexes = new()
+                                            {
+                                                6, 7, 2, 3
+                                            };
+        List<int> actualLeftIndexes = indexes["Left"];
+
+        // CollectionAssert.AreEqual(expectedLeftIndexes, actualLeftIndexes);
         return indexes;
     }
 
-    private static List<int> GetLeftIndexes(char[] input)
+    private static string GetLeftPair(char[] input, List<int> leftIndexes)
     {
-        int leftLength = GetLeftPartLength(input);
-
-        List<int> result = new();
-
-        // for (var i = 0; i <= leftLength; i++)
-        // {
-        // if (IsEven(i))
-        // {
-        // indexes["Left"].Add(evenIndexes.Pop());
-        // }
-        // else
-        // {
-        // IEnumerable<int> collection = oddIndexes.Reverse();
-        // Queue<int> reversed = new(collection);
-        // oddIndexes = new Stack<int>(reversed);
-        // indexes["Left"].Add(reversed.Dequeue());
-        // }
-        // }
-
-        // Get right indexes
-        int rightLength = input.Length - (leftLength + 1);
-
-        // for (var i = 0; i <= rightLength; i++)
-        // {
-        // if (IsEven(i))
-        // {
-        // indexes["Right"].Add(oddIndexes.Pop());
-        // }
-        // else
-        // {
-        // indexes["Right"].Add(oddIndexes.Pop());
-        // }
-        // }
-
-        // 6-7-2-3-0-1-4-5
-
-        // 2-3-
-        // 6-7-2-3-
-        // result = indexes["Left"];
-        return result;
-    }
-
-    private static string GetLeftPair(char[] input)
-    {
-        var leftPairStartIndex = 0;
         var leftPairBuilder = new StringBuilder();
 
-        List<int> leftIndexes = GetLeftIndexes(input);
-
-        // 2-3
-        // 6-7-2-3-
-        for (int i = leftIndexes.Count; i >= leftPairStartIndex; i--)
+        for (var i = 0; i < leftIndexes.Count; i++)
         {
-            int leftIndex = i;
+            int leftIndex = leftIndexes[i];
             Console.Write($"{leftIndex}-");
-            leftPairBuilder.Append(input[leftIndex]);
+            char value = input[leftIndex];
+            leftPairBuilder.Append(value);
         }
 
         var leftPair = leftPairBuilder.ToString();
         return leftPair;
-    }
-
-    private static int GetLeftPartLength(char[] input)
-    {
-        var middlePartLength = 1;
-        int restOfTheStringLength = input.Length - middlePartLength;
-        bool isLengthEven = IsEven(restOfTheStringLength);
-        var leftPartLength = 0;
-
-        if (isLengthEven)
-        {
-            leftPartLength = restOfTheStringLength / 2;
-        }
-        else
-        {
-            leftPartLength = restOfTheStringLength / 2 - 1;
-        }
-
-        return leftPartLength;
-    }
-
-    private static int GetMiddlePairIndex(char[] hexChars)
-    {
-        int index = -1;
-
-        for (var i = 0; i <= 9; i++)
-        {
-            var predicate = Convert.ToString(i);
-            index = hexChars.ToList().LastIndexOf(char.Parse(predicate));
-
-            bool indexFound = index != -1;
-            if (indexFound)
-            {
-                break;
-            }
-        }
-
-        return index;
     }
 
     private static Stack<int> GetOddIndexes(char[] input)
@@ -221,19 +178,9 @@ internal class Kata
         return oddIndexesStack;
     }
 
-    private static IEnumerable<int> GetRightIndexes(char[] input)
+    private static string GetRightPair(char[] input, List<int> rightIndexes)
     {
-        IEnumerable<int> result = GetEvenIndexes(input);
-
-        // IEnumerable<int> result = Enumerable.Range(startIndex, endIndex).Select(i => i);
-        return result;
-    }
-
-    private static string GetRightPair(char[] input, int middleCharIndex)
-    {
-        int rightPairStartIndex = middleCharIndex + 1;
         var rightPairBuilder = new StringBuilder();
-        IEnumerable<int> rightIndexes = GetRightIndexes(input);
 
         foreach (int rightIndex in rightIndexes)
         {
--- MidEndianNumbers/Tests.cs
+++ MidEndianNumbers/Tests.cs
@@ -11,38 +11,4 @@ internal class Tests
     {
         Assert.AreEqual(expected, Kata.MidEndian(input));
     }
-
-    [Test]
-    public void InputIsCorrectlyReversed()
-    {
-        throw new NotImplementedException();
-    }
-
-    [Test]
-    public void MidEndianReturns483F0DAD()
-    {
-        // actual 08FAD3D4
-        Assert.AreEqual("483F0DAD", Kata.MidEndian(0));
-    }
-
-    [Test]
-    public void MidEndianReturns4BAC06C6()
-    {
-        // actual 0BCC6A64
-        Assert.AreEqual("4BAC06C6", Kata.MidEndian(0));
-    }
-
-    [Test]
-    public void MidEndianReturnsA6E20625()
-    {
-        // actual 06226E5A
-        Assert.AreEqual("A6E20625", Kata.MidEndian(0));
-    }
-
-    [Test]
-    public void MidEndianReturnsAB05()
-    {
-        // actual 0B5A
-        Assert.AreEqual("AB05", Kata.MidEndian(0));
-    }
 }
