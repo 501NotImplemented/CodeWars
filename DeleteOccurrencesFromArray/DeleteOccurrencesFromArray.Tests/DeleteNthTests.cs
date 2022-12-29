2a1852367780f1e5d5e8cba12bb7fc93796ff218
Oleksandr Shyian <ashqa14@gmail.com>
2022-12-29 22:34:56 +0200

    Add solution

---
+++ DeleteOccurrencesFromArray/DeleteOccurrencesFromArray.Tests/DeleteNthTests.cs
@@ -0,0 +1,42 @@
+﻿using NUnit.Framework;
+
+namespace DeleteOccurrencesFromArray.Tests
+{
+    [TestFixture]
+    public class DeleteNthTests
+    {
+        [Test]
+        public void TestSimple()
+        {
+            var expected = new[]
+                               {
+                                   20, 37, 21
+                               };
+
+            var actual = Kata.DeleteNth(new[]
+                                            {
+                                                20, 37, 20, 21
+                                            },
+                1);
+
+            CollectionAssert.AreEqual(expected, actual);
+        }
+
+        [Test]
+        public void TestSimple2()
+        {
+            var expected = new[]
+                               {
+                                   1, 1, 3, 3, 7, 2, 2, 2
+                               };
+
+            var actual = Kata.DeleteNth(new[]
+                                            {
+                                                1, 1, 3, 3, 7, 2, 2, 2, 2
+                                            },
+                3);
+
+            CollectionAssert.AreEqual(expected, actual);
+        }
+    }
+}
---
+++ DeleteOccurrencesFromArray/DeleteOccurrencesFromArray/Kata.cs
@@ -0,0 +1,25 @@
+﻿namespace DeleteOccurrencesFromArray
+{
+    // https://www.codewars.com/kata/554ca54ffa7d91b236000023/train/csharp
+    public class Kata
+    {
+        public static int[] DeleteNth(int[] arr, int x)
+        {
+            List<int> occurrencesList = new();
+
+            foreach (int item in arr)
+            {
+                int itemsAlreadyInList = occurrencesList.Count(num => num.Equals(item));
+                if (itemsAlreadyInList >= x)
+                {
+                    continue;
+                }
+
+                occurrencesList.Add(item);
+            }
+
+            int[] filtered = occurrencesList.ToArray();
+            return filtered;
+        }
+    }
+}
