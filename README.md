76db0d7b89f9f811ca984ded1f717d40e0250948
Oleksandr Shyian <ashqa14@gmail.com>
2023-01-22 20:31:37 +0200

    Add Valid Parentheses

--- README.md
+++ README.md
@@ -5,6 +5,7 @@ Katas from codewars.com
 - [Mid-Endian numbers](https://www.codewars.com/kata/5b3e3ca99c9a75a62400016d)
 - [Delete occurrences of an element if it occurs more than n times](https://www.codewars.com/kata/554ca54ffa7d91b236000023)
 - [Sum of Digits / Digital Root](https://www.codewars.com/kata/541c8630095125aba6000c00)
+- [Valid Parentheses](https://www.codewars.com/kata/52774a314c2333f0a7000688)
 - Constructing a car
     - [Constructing a car #1 - Engine and Fuel Tank](https://www.codewars.com/kata/578b4f9b7c77f535fc00002f)
     - [Constructing a car #2 - Driving](https://www.codewars.com/kata/578df8f3deaed98fcf0001e9)
---
+++ ValidParentheses/ValidParentheses/Parentheses.cs
@@ -0,0 +1,53 @@
+﻿namespace ValidParentheses
+{
+    public class Parentheses
+    {
+        public static bool ValidParenthesesSequence(string input)
+        {
+            var openingBracketsAmount = 0;
+            var closingBracketsAmount = 0;
+            bool isCorrectSequence;
+            var openingBracket = '(';
+            var closingBracket = ')';
+
+            for (var i = 0; i < input.Length; i++)
+            {
+                char currentSymbol = input[i];
+                bool currentSymbolIsOpeningBracket = currentSymbol == openingBracket;
+                bool currentSymbolIsClosingBracket = currentSymbol == closingBracket;
+
+                if (!currentSymbolIsOpeningBracket && !currentSymbolIsClosingBracket)
+                {
+                    continue;
+                }
+
+                if (currentSymbolIsOpeningBracket)
+                {
+                    openingBracketsAmount = ++openingBracketsAmount;
+                }
+                else
+                {
+                    bool firstBracketIsInvalid = i == 0;
+                    if (firstBracketIsInvalid)
+                    {
+                        isCorrectSequence = false;
+                        return isCorrectSequence;
+                    }
+
+                    if (currentSymbolIsClosingBracket)
+                    {
+                        closingBracketsAmount = ++closingBracketsAmount;
+                        if (openingBracketsAmount < closingBracketsAmount)
+                        {
+                            isCorrectSequence = false;
+                            return isCorrectSequence;
+                        }
+                    }
+                }
+            }
+
+            isCorrectSequence = openingBracketsAmount == closingBracketsAmount;
+            return isCorrectSequence;
+        }
+    }
+}
