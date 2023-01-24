2f468f381e7ca7f7926e380b51f9e2a36ce2c9fc
Oleksandr Shyian <ashqa14@gmail.com>
2023-01-24 23:11:36 +0200

    Add solution

---
+++ Array.Diff/Array.Diff.Tests/SolutionTest.cs
@@ -0,0 +1,84 @@
+﻿using NUnit.Framework;
+
+namespace Array.Diff.Tests
+{
+    [TestFixture]
+    public class SolutionTest
+    {
+        [TestCase(new[]
+                      {
+                          1, 2
+                      },
+            new[]
+                {
+                    1
+                },
+            new[]
+                {
+                    2
+                })]
+        [TestCase(new[]
+                      {
+                          1, 2, 2
+                      },
+            new[]
+                {
+                    1
+                },
+            new[]
+                {
+                    2, 2
+                })]
+        [Test]
+        public void SampleTest(int[] a, int[] b, int[] expectedResult)
+        {
+            Assert.AreEqual(expectedResult, Kata.ArrayDiff(a, b));
+
+            // Assert.AreEqual(new[]
+            // {
+            // 1
+            // },
+            // Kata.ArrayDiff(new[]
+            // {
+            // 1, 2, 2
+            // },
+            // new[]
+            // {
+            // 2
+            // }));
+            // Assert.AreEqual(new[]
+            // {
+            // 1, 2, 2
+            // },
+            // Kata.ArrayDiff(new[]
+            // {
+            // 1, 2, 2
+            // },
+            // new int[]
+            // {
+            // }));
+            // Assert.AreEqual(new int[]
+            // {
+            // },
+            // Kata.ArrayDiff(new int[]
+            // {
+            // },
+            // new[]
+            // {
+            // 1, 2
+            // }));
+            // Assert.AreEqual(new[]
+            // {
+            // 3
+            // },
+            // Kata.ArrayDiff(new[]
+            // {
+            // 1, 2, 3
+            // },
+            // new[]
+            // {
+            // 1, 2
+            // }));
+        }
+    }
+}
---
+++ Array.Diff/Array.Diff/Kata.cs
@@ -0,0 +1,25 @@
+﻿namespace Array.Diff
+
+{
+    public class Kata
+    {
+        public static int[] ArrayDiff(int[] a, int[] b)
+        {
+            List<int> aList = a.ToList();
+
+            foreach (int itemA in a)
+            {
+                foreach (int itemB in b)
+                {
+                    if (itemA == itemB)
+                    {
+                        aList.Remove(itemB);
+                    }
+                }
+            }
+
+            int[] result = aList.ToArray();
+            return result;
+        }
+    }
+}
